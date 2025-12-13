2c9903b90320f47fd20f4ad7554e10a6f121ce08
jhblee <jhblee@ucdavis.edu>
2025-12-12 17:38:13 -0800

    implemented a few other animations
    fixed the issue in which the player shooting animation is not properly animated while moving, adjusted the speed of animations for each gun, added pick up animation

--- Scripts/Item/item_pickup.gd
+++ Scripts/Item/item_pickup.gd
@@ -1,35 +1,121 @@
 class_name ItemPickup
 extends Node
 
-@export var _pick_up_time:float
-@onready var _player:Player = %Player
-@onready var _crafting_handler:CraftingHandler = %CraftingHandler
-@onready var _bandage_handler:BandageHandler = %BandageHandler
-@onready var _area:ItemCollision = $Area2D
-@onready var _action_item:ActionItem = %ActionItem
+@export var _pick_up_time: float = 0.6
+@export var _pickup_anim_y_offset: float = 12.0 
 
-var _progress:float
-var _action_type:ActionItem.BarType = ActionItem.BarType.pickup
-	
-func handle_item_hover(body, delta:float) -> void:
+@onready var _player: Player = %Player
+@onready var _crafting_handler: CraftingHandler = %CraftingHandler
+@onready var _bandage_handler: BandageHandler = %BandageHandler
+@onready var _area: ItemCollision = $Area2D
+@onready var _action_item: ActionItem = %ActionItem
+@onready var _pickup_anim: AnimatedSprite2D = $PickUpAnimation
+
+const PICKUP_ANIM_NAME: StringName = &"pick_up"
+
+var _progress: float = 0.0
+var _action_type: ActionItem.BarType = ActionItem.BarType.pickup
+var _hover_item: ItemBase = null
+var _pickup_anim_speed: float = 1.0
+
+func _ready() -> void:
+	if _pickup_anim == null:
+		push_error("PickUpAnimation node not found. Fix the path to $PickUpAnimation.")
+		return
+
+	_pickup_anim.visible = false
+	_pickup_anim.animation_finished.connect(_on_pickup_anim_finished)
+
+func handle_item_hover(body, delta: float) -> void:
+	var item: ItemBase = body.get_parent() as ItemBase
+	if item == null:
+		return
+
+	if _hover_item != item:
+		_hover_item = item
+		_progress = 0.0
+		_start_pickup_vfx(item)
+
+	_update_pickup_vfx_pos(item)
+
 	_progress += delta
+
 	if _progress >= _pick_up_time:
 		_action_item.conclude_action_bar(_action_type)
-		var item:ItemBase = body.get_parent()
+
 		if item.item_type == ItemBase.ItemType.scrap:
 			_crafting_handler.pick_up_scrap()
 		elif item.item_type == ItemBase.ItemType.gun:
-			_player.pick_up_gun(body.get_parent().get_gun_type())
+			_player.pick_up_gun(item.get_gun_type())
 		elif item.item_type == ItemBase.ItemType.bandage:
 			_bandage_handler.add_bandage(1)
-			
+
 		_area.reset(body)
 		item.delete_entity()
-		_progress = 0
+
+		_progress = 0.0
+		_hover_item = null
 	else:
-		var _ratio:float = _progress / _pick_up_time
-		_action_item.set_action_bar(_ratio, _action_type)
+		var ratio: float = _progress / _pick_up_time
+		_action_item.set_action_bar(ratio, _action_type)
 
 func handle_item_exit() -> void:
 	_action_item.conclude_action_bar(_action_type)
-	_progress = 0
+	_progress = 0.0
+	_hover_item = null
+	_stop_pickup_vfx()
+
+func _start_pickup_vfx(item: ItemBase) -> void:
+	if _pickup_anim == null:
+		return
+
+	_pickup_anim_speed = _compute_speed_to_match_time(PICKUP_ANIM_NAME, _pick_up_time)
+	_pickup_anim.visible = true
+	_pickup_anim.set_frame_and_progress(0, 0.0)
+	_update_pickup_vfx_pos(item)
+
+
+	_pickup_anim.play(PICKUP_ANIM_NAME, _pickup_anim_speed)
+
+func _update_pickup_vfx_pos(item: ItemBase) -> void:
+	if _pickup_anim == null:
+		return
+	_pickup_anim.global_position = item.global_position + Vector2(0.0, -_pickup_anim_y_offset)
+
+func _stop_pickup_vfx() -> void:
+	if _pickup_anim == null:
+		return
+	_pickup_anim.visible = false
+	_pickup_anim.stop()
+
+func _on_pickup_anim_finished() -> void:
+
+	if _hover_item == null:
+		_stop_pickup_vfx()
+
+func _compute_speed_to_match_time(anim_name: StringName, desired_time: float) -> float:
+
+	if _pickup_anim == null or desired_time <= 0.0:
+		return 1.0
+
+	var sf: SpriteFrames = _pickup_anim.sprite_frames
+	if sf == null or not sf.has_animation(anim_name):
+		return 1.0
+
+	var fps: float = sf.get_animation_speed(anim_name)
+	if fps <= 0.0:
+		return 1.0
+
+	var frame_count: int = sf.get_frame_count(anim_name)
+	if frame_count <= 0:
+		return 1.0
+
+	var sum_rel: float = 0.0
+	for i in range(frame_count):
+		sum_rel += sf.get_frame_duration(anim_name, i)
+
+	var base_duration: float = sum_rel / fps
+	if base_duration <= 0.0:
+		return 1.0
+
+	return base_duration / desired_time
--- Scripts/animation_handler.gd
+++ Scripts/animation_handler.gd
@@ -5,8 +5,12 @@ extends Node2D
 @onready var _animated_sprite:AnimatedSprite2D = $PlayerAnimations
 
 var _is_walking:bool = false
-#var _is_shooting:bool = false
-#var _is_healing:bool = false
+var _is_healing:bool = false
+var _is_shooting:bool = false
+
+var _queued_walk:bool = false
+var _pending_shot:bool = false
+var _current_action:ActionType = ActionType.idle
 
 enum ActionType {
 	idle,
@@ -15,66 +19,115 @@ enum ActionType {
 	heal
 }
 
+func _ready() -> void:
+	_animated_sprite.animation_finished.connect(_on_animation_finished)
+
 func make_player_idle() -> void:
+	if _is_healing or _is_shooting:
+		_is_walking = false
+		return
+
 	_is_walking = false
-	
+	_current_action = ActionType.idle
 	_play_animation(ActionType.idle)
 
 func make_player_walk() -> void:
+	if _is_healing:
+		_queued_walk = true
+		return
+
+	if _is_shooting:
+		_is_walking = true
+		return
+
 	if _is_walking:
 		return
-	
+
 	_is_walking = true
+	_current_action = ActionType.walk
 	_play_animation(ActionType.walk)
 
 func make_player_shoot() -> void:
+	if _is_healing:
+		return
+
+	if _is_shooting:
+		_pending_shot = true
+		return
+
+	_is_shooting = true
+	_current_action = ActionType.shoot
 	_play_animation(ActionType.shoot)
 
 func make_player_heal() -> void:
+	if _is_healing:
+		return
+
+	_is_healing = true
+	_is_shooting = false
+	_pending_shot = false
+
+	_queued_walk = false
+	_is_walking = false
+	_current_action = ActionType.heal
 	_play_animation(ActionType.heal)
 
 func _play_animation(action:ActionType) -> void:
-	var should_loop:bool = _get_loop_stauts(action)
 	var animation_name:String = _get_animation_weapon() + "_" + _get_animation_action(action)
-	_animated_sprite.play(animation_name, should_loop)
+	var should_loop:bool = _get_loop_stauts(action)
+
+	_animated_sprite.sprite_frames.set_animation_loop(animation_name, should_loop)
+
+	if not should_loop:
+		_animated_sprite.stop()
+		_animated_sprite.frame = 0
+
+	_animated_sprite.play(animation_name)
 
 func _get_animation_action(action:ActionType) -> String:
 	match (action):
-		ActionType.idle:
-			return "idle"
-		ActionType.walk:
-			return "walk"
-		ActionType.shoot:
-			return "shoot"
-		ActionType.heal:
-			return "heal"
-	
+		ActionType.idle: return "idle"
+		ActionType.walk: return "walk"
+		ActionType.shoot: return "shoot"
+		ActionType.heal: return "heal"
 	return ""
 
 func _get_animation_weapon() -> String:
 	match (_weapon_handler.equipted_weapon):
-		WeaponHandler.WeaponType.pistol:
-			return "pistol"
-		WeaponHandler.WeaponType.smg:
-			return "smg"
-		WeaponHandler.WeaponType.assault:
-			return "assault"
-		WeaponHandler.WeaponType.sniper:
-			return "sniper"
-	
+		WeaponHandler.WeaponType.pistol: return "pistol"
+		WeaponHandler.WeaponType.smg: return "smg"
+		WeaponHandler.WeaponType.assault: return "assault"
+		WeaponHandler.WeaponType.sniper: return "sniper"
 	return ""
 
 func _get_loop_stauts(action:ActionType) -> bool:
 	match (action):
-		ActionType.idle:
-			return true
-		ActionType.walk:
-			return true
-		ActionType.shoot:
-			return false
-		ActionType.heal:
-			return false
-	
+		ActionType.idle: return true
+		ActionType.walk: return true
+		ActionType.shoot: return false
+		ActionType.heal: return false
 	return false
 
-	
+func _on_animation_finished() -> void:
+	if _current_action == ActionType.heal:
+		_is_healing = false
+		if _queued_walk:
+			_queued_walk = false
+			make_player_walk()
+		else:
+			make_player_idle()
+		return
+
+	if _current_action == ActionType.shoot:
+		if _pending_shot:
+			_pending_shot = false
+			_current_action = ActionType.shoot
+			_play_animation(ActionType.shoot)
+			return
+
+		_is_shooting = false
+		if _is_walking:
+			make_player_walk()
+		else:
+			make_player_idle()
+		return
